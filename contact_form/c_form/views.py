0a53033a70ef256a5de8f75cd4b267bff2f8ebca
ritikgarg655 <18k009@nirmauni.ac.in>
2020-05-08 18:02:00 +0530

    Redirecting working

--- contact_form/c_form/views.py
+++ contact_form/c_form/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse ,HttpResponseRedirect
 from django.shortcuts import render,get_object_or_404,redirect
 from django.shortcuts import render
 from .models import contact_form
@@ -7,16 +7,22 @@ from .forms import form_input
 
 def create_form(request):
 	form = form_input()
+	
 	if request.method == 'POST' and 'contact_form' in request.POST:
+		
 		form = form_input(request.POST)
+		
 		if form.is_valid():
-			contact_form.objects.create(**form.cleaned_data)
-			all_form = contact_form.objects.all()
-			last_form = all_id[-1]
-			last_id = last_form.id
-			return redirect('contact_form/view/'+last_id)
+			
+			f = contact_form.objects.create(**form.cleaned_data)
+			last_id = f.id
+			
+			return HttpResponseRedirect	('../view/'+str(last_id))
+		
 		else:
+			
 			print("invalid input")
+	
 	context = {
 		"input_form" : form
 	}
@@ -24,13 +30,17 @@ def create_form(request):
 	return render(request,"create_form.html",context)
 
 def view(request):
+	
 	details = contact_form.objects.all()
+	
 	context = {
 		'details' : details
 	}
+	
 	return render(request,"view.html",context)
 
 def view_id(request,my_id):
+	
 	details = get_object_or_404(contact_form,id=my_id)
 
 	context = {
